cb521b10e4bbd29a443929e7dc95494764cf8cc3
haruhiko <haruhiko@weseek.co.jp>
2020-07-08 15:25:57 +0900

    - デバッグ環境整備

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,12 @@ import main from "../scraping/zoushigaya"
 const router = Express.Router();
 
 router.get('/', async (req, res) => {
-    res.render("index", {message: 'message'});
-  })
+  res.render("index", {message: 'message'});
+})
 
 router.get('/zoushigaya', async (req, res) => {
-    await main();
-    res.render("zoushigaya/index");
-  })
+  await main();
+  res.render("zoushigaya/index");
+})
 
 export default router;
