17f30dfc995ae17dee9a93396245a9f154779463
haruhiko <haruhiko@weseek.co.jp>
2020-07-10 15:39:03 +0900

    - bootstrap仮導入 - 月の選択

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,8 +9,11 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/zoushigaya', async (req, res) => {
-  await main('08', '01');
-  res.render("zoushigaya/index");
+  const month = req.query.month as string
+  if (month) {
+    await main(month, '01');
+  }
+  res.render("zoushigaya/index", { month });
 })
 
 export default router;
