b89acec379627ae60a0c14aaacdc83184fcb75aa
大谷東彦 <haruhiko@weseek.co.jp>
2020-07-14 00:33:12 +0900

    - 江東区

--- src/scraping/koto-ku.ts
+++ src/scraping/koto-ku.ts
@@ -10,7 +10,7 @@ const get = async (month: string, day: string) => {
   const url = 'https://sisetun.kcf.or.jp/web/'
   await page.goto(url, { waitUntil: 'networkidle0' });
 
-  // 検索メニュー
+  // 施設の空き状況
   let loadPromise = page.waitForNavigation();
   // @ts-ignore
   await page.evaluate(() => doAction(((_dom == 3) ? document.layers['disp'].document.formWTransInstSrchVacantAction : document.formWTransInstSrchVacantAction ), gRsvWTransInstSrchVacantAction))
@@ -21,78 +21,66 @@ const get = async (month: string, day: string) => {
   // @ts-ignore
   await page.evaluate(() => doAction((_dom == 3) ? document.layers['disp'].document.formWTransInstSrchMultipleAction : document.formWTransInstSrchMultipleAction, gRsvWTransInstSrchMultipleAction))
   await loadPromise;
+
+  // 利用目的
+  loadPromise = page.waitForNavigation();
+  // @ts-ignore
+  await page.evaluate(() => sendSelectWeekNum((_dom == 3) ? document.layers['disp'].document.formWTransInstSrchMultipleAction : document.formWTransInstSrchMultipleAction, gRsvWTransInstSrchPpsAction))
+  await loadPromise;
+
+  // オーケストラ・楽団
+  loadPromise = page.waitForNavigation();
+  // @ts-ignore
+  await page.evaluate(() => sendPpsCd((_dom == 3) ? document.layers['disp'].document.formWTransInstSrchMultipleAction : document.formWTransInstSrchMultipleAction, gRsvWTransInstSrchMultipleAction, '120' , '12020'))
+  await loadPromise;
+
+  // 年月日
+  loadPromise = page.waitForNavigation();
+  // @ts-ignore
+  await page.evaluate(() => sendSelectWeekNum((_dom == 3) ? document.layers['disp'].document.formWTransInstSrchMultipleAction : document.formWTransInstSrchMultipleAction, gRsvWTransInstSrchSetDayAction))
+  await loadPromise;
+
+  // 開始年月日 選択
+  // @ts-ignore
+  await page.evaluate((month, day) => changeDayGif((_dom == 3) ? document.layers['disp'].document.CalendarDays16 : document.CalendarDays16, 2020, month, day), month, day)
+
+  // 年月日設定
+  loadPromise = page.waitForNavigation();
+  // @ts-ignore
+  await page.evaluate(() => sendSelectDay((_dom == 3) ? document.layers['disp'].document.formCommonSrchDayWeekAction : document.formCommonSrchDayWeekAction, gRsvWTransInstSrchMultipleAction, 1))
+  await loadPromise;
+
+  // 土 日 祝 選択
+  await page.evaluate(() => { 
+    // @ts-ignore
+    changeWeekGif((_dom == 3) ? document.layers['disp'].document.weektype5 : document.weektype5, 5)
+    // @ts-ignore
+    changeWeekGif((_dom == 3) ? document.layers['disp'].document.weektype6 : document.weektype6, 6)
+    // @ts-ignore
+    changeWeekGif((_dom == 3) ? document.layers['disp'].document.weektype7 : document.weektype7, 7)
+  })
+
+  // 検索開始
+  loadPromise = page.waitForNavigation();
+  // @ts-ignore
+  await page.evaluate(() => sendSelectWeekNum((_dom == 3) ? document.layers['disp'].document.formWTransInstSrchMultipleAction : document.formWTransInstSrchMultipleAction, gRsvWGetInstSrchInfAction))
+  await loadPromise;
+
+  // 江東区文化センターホール
+  await page.screenshot({ path: `screenshots/koto-ku/${month}_${day}_99.png`, fullPage: true });
+
+
+
+  for (let i = 1; i < 20; ++i) {
+    // 次の施設へループ
+    loadPromise = page.waitForNavigation();
+    // @ts-ignore
+    await page.evaluate(() => doTransInstSrchVacantTzoneAction((_dom == 3) ? document.layers['disp'].document.formWTransInstSrchVacantTzoneAction : document.formWTransInstSrchVacantTzoneAction, gRsvWTransInstSrchVacantAction, 6, gSrchSelectInstNo, gSrchSelectInstMax))
+    await loadPromise;
   
-  // // ログイン
-  // loadPromise = page.waitForNavigation();
-  // await page.type('#txtID', process.env.ZOUSHIGAYA_ID);
-  // await page.type('#txtPass', process.env.ZOUSHIGAYA_PASS);
+    await page.screenshot({ path: `screenshots/koto-ku/${month}_${day}_${i}.png`, fullPage: true });
+  }
 
-  // await page.click('#ucPCFooter_pnlNextBtn')
-  // await loadPromise;
-
-  // // 雑司が谷文化
-  // loadPromise = page.waitForNavigation();
-  // await page.click('#dlSSCategory_ctl03_btnSSCategory')
-  // await loadPromise;
-
-  // loadPromise = page.waitForNavigation();
-  // await page.click('#dgShisetsuList_ctl04_chkSelectLeft')
-  // await page.click('#ucPCFooter_btnForward');
-  // await loadPromise;
-  
-  // // 日時選択
-  // loadPromise = page.waitForNavigation();
-  // await page.click('#rbtnMonth');
-  // await page.click('#chkSat');
-  // await page.click('#chkSun');
-  // await page.click('#chkHol');
-  
-  // await page.$eval('#txtMonth',  (element, month) => (element as HTMLInputElement).value = month, month);
-  // await page.$eval('#txtDay', (element, day) => (element as HTMLInputElement).value = day, day);
-  // await page.click('#ucPCFooter_btnForward');
-  // await loadPromise;
-
-
-  // const days = []
-  // for (let i = 0; i < 15; ++i) {
-  //   const dayElement = await page.$(`#dlRepeat_ctl00_tpItem_dgTable > tbody > tr.TitleColor > td:nth-child(${i + 3})`)
-  //   if (!dayElement) { break }
-  //   const day = await dayElement.getProperty('textContent')
-  //   days.push(((await day.jsonValue()) as string).slice( 0, -1 ))
-  // }
-
-  // for (const roomId of roomIds) {
-  //   console.log(`roomId: ${roomId} start`)
-  //   // 更新ボタンを押す
-  //   loadPromise = page.waitForNavigation();
-  //   await page.click('#btnUpdate');
-  //   await loadPromise;
-
-  //   // 0埋めしてチェックループ
-  //   for (const day of days.map(d => ( '0' + d ).slice( -2 ))) {
-  //     try {
-  //       // 最大20個までしかチェックができない
-  //       await page.click(`#dlRepeat_ctl00_tpItem_dgTable_ctl${roomId}_b2020${month}${day}`);
-  //     } catch (error) {
-  //       console.log('skip:', error)
-  //     }
-  //   }
-    
-  //   // 空き情報確認
-  //   loadPromise = page.waitForNavigation();
-  //   await page.click('#ucPCFooter_btnForward');
-  //   await loadPromise;
-
-  //   await page.screenshot({ path: `screenshots/zoushigaya/${roomId}_${month}.png`, fullPage: true });
-  //   console.log(`roomId: ${roomId} end`)
-
-  //   // 戻る
-  //   loadPromise = page.waitForNavigation();
-  //   await page.click('#ucPCFooter_btnBack');
-  //   await loadPromise;
-    
-  // }
-  await page.screenshot({ path: `screenshots/koto-ku/${month}_${day}.png`, fullPage: true });
 
   await browser.close();
   console.log('koto-ku end')
