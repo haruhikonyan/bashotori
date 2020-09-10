6a9e369d7f678ed915ec50f5b30dd118ddee1221
haruhiko <haruhiko@weseek.co.jp>
2020-09-10 13:46:41 +0900

    - linebot 仮追加

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,19 @@ import * as Express from 'express';
 import zoushigaya from "../scraping/zoushigaya"
 import kotoKu from "../scraping/koto-ku"
 import sumidaKu from "../scraping/sumida-ku"
+import { Client, middleware, ClientConfig, MiddlewareConfig } from "@line/bot-sdk";
 
 const router = Express.Router();
+
+let lineConfig: ClientConfig | MiddlewareConfig
+let lineBot
+if (process.env.LINE_ACCESS_TOKEN && process.env.LINE_CHANNEL_SECRET) {
+  lineConfig = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+  }
+  lineBot = new Client(lineConfig as ClientConfig)
+}
 
 router.get('/', async (req, res) => {
   res.render("index", {message: 'message'});
@@ -56,8 +67,30 @@ router.get('/sumida-ku', async (req, res) => {
   res.render("sumida-ku/index", { month, buildingkeys, error });
 })
 
-router.get('/webhook', async (req, res) => {
-  res.json({ webhook: 'test' });
+router.get('/webhook', middleware(lineConfig as MiddlewareConfig), async (req, res) => {
+  // 先行してLINE側にステータスコード200でレスポンスする。
+  res.sendStatus(200);
+
+  // すべてのイベント処理のプロミスを格納する配列。
+  const events_processed: Promise<any>[] = [];    
+  // イベントオブジェクトを順次処理。
+  req.body.events.forEach((event) => {
+    // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+    if (event.type == "message" && event.message.type == "text"){
+      // ユーザーからのテキストメッセージが「ヘルプ」だった場合のみ反応。
+      if (event.message.text == "bashotoriヘルプ"){
+        // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+        events_processed.push(lineBot.replyMessage(event.replyToken, {
+          type: "text",
+          text: "bashotori のヘルプです"
+        }));
+      }
+    }
+  });
+
+  // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+  const response = await Promise.all(events_processed)
+  console.log(`${response.length} event(s) processed.`)
 })
 
 export default router;
